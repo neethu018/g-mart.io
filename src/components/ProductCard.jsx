72e6f752b69d058af139790f05e21cba4b602cda
blackboytechie <138457971+Blackboytechie@users.noreply.github.com>
2024-01-04 23:01:16 +0530

    initiated categoryaccordians components

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,29 +1,28 @@
-
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Card, Button } from "react-bootstrap";
 import { Link, useNavigate, useLocation } from "react-router-dom";
-import { CartContext } from '../context/CartContext'
+import { CartContext } from "../context/CartContext";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
-import { useContext } from 'react'
+import { useContext } from "react";
 
 const ProductCard = ({ product, category, subcategory }) => {
   const { pathname } = useLocation();
-  const { cartItems, addToCart } = useContext(CartContext)
+  const { cartItems, addToCart } = useContext(CartContext);
   const navigate = useNavigate();
 
   let path = "";
   if (subcategory) {
     path = `${product.productId}`;
   } else if (category === "allproducts") {
-    path = `${category}/${product.shortDesc.split(" ")[0]}/${product.productId}`;
+    path = `${category}/${product.shortDesc.split(" ")[0]}/${
+      product.productId
+    }`;
   } else if (category) {
     path = `${product.shortDesc.split(" ")[0]}/${product.productId}`;
   } else {
     navigate("/");
   }
-  
 
   const handleAddToCart = () => {
     // Call the function from useCart to add the product to the cart
@@ -32,22 +31,18 @@ const ProductCard = ({ product, category, subcategory }) => {
     addToCart(product);
     // console.log("Cart after adding:", cartItems); // Make sure this log shows the updated cart
     // console.log("add to cart button clicked")
-
   };
 
   return (
-    <Card
-      style={{ borderRadius: "10%", padding: 20, marginBottom: 20 }}
-      as={Link}
-      to={`${pathname}/${path}`}
-      state={{ product }}
-    >
-      <Card.Img
-        variant="top"
-        src={product.productImage[0]}
-        alt={product.productImage}
-        style={{ width: 300, height: 300 }}
-      />
+    <Card style={{ borderRadius: "10%", padding: 20, marginBottom: 20 }}>
+      <Link to={`${pathname}/${path}`} state={{ product }}>
+        <Card.Img
+          variant="top"
+          src={product.productImage[0]}
+          alt={product.productImage}
+          style={{ width: 200, height: 200 }}
+        />
+      </Link>
       <Card.Title style={{ position: "absolute", top: 30, right: 30 }}>
         <FontAwesomeIcon
           icon={regularHeart}
@@ -57,8 +52,23 @@ const ProductCard = ({ product, category, subcategory }) => {
       <Card.Body>
         <Card.Title>{product.shortDesc}</Card.Title>
         <Card.Text>
-          {(product.mrp - product.mrp * (product.discount / 100)).toFixed()}{" "}
-          {product.mrp} {product.discount}% OFF
+        {product.discount > 0 ? (
+            <div className="d-flex flex-row justify-content-start gap-2">
+              <p style={{ fontWeight: "bold" }}>
+                &#x20B9;{" "}
+                {(
+                  product.mrp -
+                  product.mrp * (product.discount / 100)
+                ).toFixed()}
+              </p>
+              <p style={{ textDecoration: "line-through" }}>
+                M.R.P:{product.mrp}
+              </p>
+              <p style={{ color: "green",backgroundColor: "#ABEBC6",borderRadius:"5px",padding:"0px 5px" }}>{product.discount}% off</p>
+            </div>
+          ) : (
+            <p style={{ fontWeight: "bold" }}>&#x20B9; {product.mrp}</p>
+          )}
         </Card.Text>
         <Button className="rounded-lg bg-white w-100" onClick={handleAddToCart}>
           <div
--- src/pages/MainCategory.jsx
+++ src/pages/MainCategory.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import ProductCard from "../components/ProductCard";
-import {useLocation} from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 import BreadCrumbs from "../components/BreadCrumbs";
+import CategoryAccordions from "../components/CategoryAccordions";
 
 const MainCategory = ({ products }) => {
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
   console.log("path", pathname);
   //breadcrumbs
   const mainTitle = pathname?.substring(1);
   const mainUrl = pathname;
-  
+
   const filteredProducts = [].concat(...Object.values(products));
   console.log("filteredProducts", filteredProducts);
 
@@ -20,26 +21,27 @@ const MainCategory = ({ products }) => {
 
   console.log("Filtered products : ", filteredProducts);
 
-  
-
   function shuffleArray(array) {
     // Create a copy of the original array to avoid modifying the original array
     const shuffledArray = [...array];
-  
+
     // Fisher-Yates Shuffle Algorithm
     for (let i = shuffledArray.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
     }
-  
+
     return shuffledArray;
   }
-  
+
   // Example usage:
   const shuffledArray = shuffleArray(filteredProducts);
-  
+
   console.log(shuffledArray);
-  
+
   return (
     <Container className="product-list-container">
       {/* First Row with Breadcrumbs */}
@@ -50,11 +52,24 @@ const MainCategory = ({ products }) => {
       </Row>
       {/* Second Row with Two Columns */}
       <Row>
-        {shuffledArray.map((product) => (
-          <Col key={product.productId} xs={12} md={6} lg={4}>
-            <ProductCard key={product.productId} product={product} category={"allproducts"} />
-          </Col>
-        ))}
+        {/* Left Column with Category Accordions */}
+        <Col md={3} lg={3}>
+          <CategoryAccordions mainCategory={mainTitle} />
+        </Col>
+        {/* Right column */}
+        <Col md={9}>
+          <Row className=" mt-3 ">
+            {shuffledArray.map((product) => (
+              <Col key={product.productId} xs={12} md={6} lg={4}>
+                <ProductCard
+                  key={product.productId}
+                  product={product}
+                  category={"allproducts"}
+                />
+              </Col>
+            ))}
+          </Row>
+        </Col>
       </Row>
     </Container>
   );
